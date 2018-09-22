f46e3f5ecdca5b56f7c3a8ef9a078d92884e7f18
sollinio <fadyzed@gmail.com>
2018-09-22 20:00:59 +0300

    kommit

--- laboratory first/phpsite/JS/slider.js
+++
@@ -1,6 +0,0 @@
-$(document).ready()
-$(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: "slide"
-  });
-});
--- laboratory first/phpsite/blocks/bd.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db = mysql_connect ("localhost","php","12345");
-mysql_select_db("phpsite",$db);
-?>
